620f38eaf081a54a481fbed36c37d1a56430cfc5
sayeedofficial <sayeedofficial6220@gmail.com>
2021-12-29 15:08:59 +0530

    Created a transfer event for Transfer and Approve of tether

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Tether {
@@ -5,4 +6,27 @@ contract Tether {
     string public symbol = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000;
     uint8 public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+    mapping(address => uint256) public balanceOf;
+
+    constructor(){
+        balanceOf(msg.sender) = totalSupply;
+    }
+    function transfer(address _to,uint _value) public returns (bool success){
+        require(balanceOf[msg.sender]>=_value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
