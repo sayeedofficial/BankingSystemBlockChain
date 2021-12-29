8b499caf7ea5cc74f5c8bb52dc70ac11bb668756
sayeedofficial <sayeedofficial6220@gmail.com>
2021-12-29 16:01:40 +0530

    Created Transfer from event and approve funtion for smart contract access

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -12,21 +12,36 @@ contract Tether {
         address indexed _to,
         uint _value
     );
-    event Approve(
+    event Approval(
         address indexed _owner,
         address indexed _spender,
         uint _value
     );
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(){
-        balanceOf(msg.sender) = totalSupply;
+        balanceOf[msg.sender] = totalSupply;
     }
-    function transfer(address _to,uint _value) public returns (bool success){
+    function transfer(address _from,address _to,uint _value) public returns (bool success){
         require(balanceOf[msg.sender]>=_value);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(_from, _to, _value);
+        return true;
+    }
+	function approve(address _spender,uint256 _value) public returns (bool success){
+		allowance[msg.sender][_spender] = _value;
+	}
+
+    function transferFrom(address _from,address _to,uint256 _value) public returns (bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_to] += _value;
+        balanceOf[_from] -= _value;
+        allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from,_to,_value);
         return true;
     }
 }
