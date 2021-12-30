c8d09af0cd62fdeb103d400c563419cdd906f961
sayeedofficial <sayeedofficial6220@gmail.com>
2021-12-30 10:26:35 +0530

    Implement Smart Contract Transactions between each other

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
-const Tether = artifacts.require("Migrations");
+const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer,networks,accounts) {
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed()
+
   await deployer.deploy(RWD);
-  await deployer.deploy(DecentralBank);
+  const rwd = await RWD.deployed();
+
+  await deployer.deploy(DecentralBank,rwd.address,tether.address);
+  const decentralBank = await DecentralBank.deployed()
+  await rwd.transfer(decentralBank.address,"10000000000000000000000")
+  await tether.transfer(accounts[1],"10000000000000000",decentralBank.address)
 };
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,8 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
+
+
+import "./RWD.sol";
+import "./Tether.sol";
 
 contract DecentralBank {
     string public name = "DecentralBank";
     address public owner;
+    RWD public rwd;
+    Tether public tether;
+
+    constructor(RWD _rwd,Tether _tether) public {
+        rwd = _rwd;
+        tether = _tether;
+    }
     
      
    
